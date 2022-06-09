cb774f918ef65e2f833a1499b6c038d703ef0537
Cup of Coding <anne@cupofcoding.com>
2022-06-08 20:10:12 -0700

    ....need to figure routes and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
-
-import Home from "./pages/Home";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
-// import Cart from "./pages/Cart";
-
-=======
 // run npm i express
 // run npm express mongoose dotenv nodemon
 // dotenv for websecurity to avoid other users from charging the stripe API
@@ -18,96 +7,45 @@ import Home from "./pages/Home";
 // run npm install @mui/icons-material
 // run npm install @mui/material
 
-import Product from "./pages/Product";
 import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import Success from "./pages/Success";
-import { useSelector } from "react-redux";
->>>>>>> 89802d6418ebb3e911be0a5b0d8436dfa3b09e12
+// import Product from "./pages/Product";
+// import ProductList from "./pages/ProductList";
+// import Cart from "./pages/Cart";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect,
+// } from "react-router-dom";
 
 const App = () => {
-<<<<<<< HEAD
   return <Home/>; 
   // return <Cart />
   // return <Register />
   // return <Login />
   // return <Product />
-  // return <ProductList/>;
-  // return (
-    
-  // (<Router>
-  //   <div>
-  //     <Routes>
-  //       <Route path='./pages/Home' element={<Home/>} />
-  //       <Route path='./pages/Product"' element={<Product />} />
-  //       <Route path='./pages/ProductList' element={<ProductList/>} />
-  //       <Route path='./pages/Register' element={<Register />} />
-  //       <Route path='./pages/Login' element={<Login />} />
-  //       <Route path='./pages/Cart' element={<Cart />} />
-  //     </Routes>
-  //   </div>
-  // </Router>
-  // );  
-=======
-  const user = useSelector((state) => state.user.currentUser);
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/products/:category">
-          <ProductList />
-        </Route>
-        <Route path="/product/:id">
-           <Product />
-         </Route>
-         <Route path="/cart">
-           <Cart />
-         </Route>
-         <Route path="/success">
-           <Success />
-         </Route>
-         <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
-         <Route path="/register">
-           {user ? <Redirect to="/" /> : <Register />}
-         </Route>
-       </Switch>
-     </Router>
-  );
->>>>>>> 89802d6418ebb3e911be0a5b0d8436dfa3b09e12
+  // return <ProductList/>;    
 };
 
-export default App;
-
-
-// //  ---------------------------------------------------
-
-// // import Product from "./pages/Product";
-// import Home from "./pages/Home";
-// // import ProductList from "./pages/ProductList";
-// // import Register from "./pages/Register";
-// // import Login from "./pages/Login";
-// // import Cart from "./pages/Cart";
-
-// // Sets home page and routing
-// const App = () => {
-//   // return <Cart />
-//   // return <Login />
-//   // return <Product />
-//   // return <ProductList/>;
-//   return <Home/>;
-//   //   <div>Hello World!</div>;
-  
+// const Home = () => {
+  // return <Home/>; 
+//   return (    
+//     <>
+//       <Router>
+//         <Switch>
+//           <Route path='./pages/Home' component={<Home/>} />
+//           <Route path='./pages/Product"' component={<Product />} />
+//           <Route path='./pages/ProductList' component={<ProductList/>} />
+//           <Route path='./pages/Register' component={<Register />} />
+//           <Route path='./pages/Login' component={<Login />} />
+//           <Route path='./pages/Cart' component={<Cart />} />
+//           <Redirect to="/">
+//         </Switch>
+//       </Router>
+//     </>
+//     );  
 // };
 
-// export default App;
+export default App;
